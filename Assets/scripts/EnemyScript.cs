5bc6a01c1fbf84f0cea83be40456aa3d0ee35404
KotoriYousan <kurosawa1997@gmail.com>
2018-06-26 15:52:54 +0800

    <add> assets

---
+++ Assets/scripts/EnemyScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour {
+
+    private WeaponScript[] weapons;
+
+    private void Awake()
+    {
+        weapons = GetComponentsInChildren<WeaponScript>();
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        foreach (WeaponScript weapon in weapons)
+        {
+            // Auto-fire
+            if (weapon != null && weapon.CanAttack)
+            {
+                weapon.Attack(true);
+            }
+        }
+    }
+}
---
+++ Assets/scripts/PlayerScript.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+
+    public Vector2 speed = new Vector2(50, 50);
+
+    private Vector2 movement;
+    private Rigidbody2D rigidbodyComponent;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float inputX = Input.GetAxis("Horizontal");
+        float inputY = Input.GetAxis("Vertical");
+
+        movement = new Vector2(
+            speed.x * inputX,
+            speed.y * inputY);
+
+        bool shoot = Input.GetButtonDown("Fire1");
+        shoot |= Input.GetButtonDown("Fire2");
+        // Careful: For Mac users, ctrl + arrow is a bad idea
+
+        if (shoot)
+        {
+            WeaponScript weapon = GetComponent<WeaponScript>();
+            if (weapon != null)
+            {
+                // false because the player is not an enemy
+                weapon.Attack(false);
+            }
+        }
+
+    }
+
+    private void FixedUpdate()
+    {
+        if (rigidbodyComponent == null) rigidbodyComponent = GetComponent<Rigidbody2D>();
+
+        rigidbodyComponent.velocity = movement;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        bool damagePlayer = false;
+
+        // Collision with enemy
+        EnemyScript enemy = collision.gameObject.GetComponent<EnemyScript>();
+        if (enemy != null)
+        {
+            // Kill the enemy
+            HealthScript enemyHealth = enemy.GetComponent<HealthScript>();
+            if (enemyHealth != null) enemyHealth.Damage(enemyHealth.hp);
+
+            damagePlayer = true;
+        }
+
+        // Damage the player
+        if (damagePlayer)
+        {
+            HealthScript playerHealth = this.GetComponent<HealthScript>();
+            if (playerHealth != null) playerHealth.Damage(1);
+        }
+    }
+}
